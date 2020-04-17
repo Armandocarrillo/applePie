504409d67108441993ce724e6e46c0cb62a5f957
Armandocarrillo <ingcompuarmando@gmail.com>
2020-04-16 21:29:56 -0500

    buttons tapped

--- applePie/ViewController.swift
+++ applePie/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var treeImage: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]! // outlet collection
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
